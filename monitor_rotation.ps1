18be56b51282eca42eef37eaea60f95dafbf859e
Lord-Topa <topavelal@gmail.com>
2023-09-26 18:40:29 -0700

    Implemented a bunch of scripts

---
+++ monitor_rotation.ps1
@@ -0,0 +1,48 @@
+﻿Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class DisplaySettings
+{
+    [DllImport("user32.dll")]
+    public static extern int EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+
+    [DllImport("user32.dll")]
+    public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DEVMODE
+    {
+        private const int DMDO_DEFAULT = 0;
+        private const int DMDO_90 = 1;
+        private const int DMDO_180 = 2;
+        private const int DMDO_270 = 3;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmDeviceName;
+        public short dmSpecVersion;
+        public short dmDriverVersion;
+        public short dmSize;
+        public short dmDriverExtra;
+        public int dmFields;
+        public int dmPositionX;
+        public int dmPositionY;
+        public int dmDisplayOrientation;
+        public int dmDisplayFixedOutput;
+    }
+
+    public static void SetScreenRotation(int rotation)
+    {
+        DEVMODE dm = new DEVMODE();
+        dm.dmDeviceName = new String(new char[32]);
+        dm.dmSize = (short)Marshal.SizeOf(dm);
+
+        EnumDisplaySettings(null, -1, ref dm);
+        dm.dmDisplayOrientation = rotation;
+
+        ChangeDisplaySettings(ref dm, 0x01);
+    }
+}
+"@
+
+[DisplaySettings]::SetScreenRotation(1)
---
+++ rdpscript.ps1
@@ -0,0 +1,32 @@
+﻿# Store the registry keys in variables for easier reuse
+$terminalServerKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
+$rdpTcpKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
+
+# Store the firewall rule groups in variables for easier reuse
+$remotefxRuleGroup = 'remote desktop - remotefx'
+$remoteDesktopRuleGroup = 'remote desktop'
+
+# Check the current value of the fDenyTSConnections property
+$currentFDenyTSConnections = Get-ItemProperty -Path $terminalServerKey -Name fDenyTSConnections
+
+# Set the value of the fDenyTSConnections property to 0 if it is not already set to that value
+if ($currentFDenyTSConnections -ne 0) {
+    Set-ItemProperty -Path $terminalServerKey -Name fDenyTSConnections -Value 0
+}
+
+# Check the current value of the UserAuthentication property
+$currentUserAuthentication = Get-ItemProperty -Path $rdpTcpKey -Name UserAuthentication
+
+# Set the value of the UserAuthentication property to 1 if it is not already set to that value
+if ($currentUserAuthentication -ne 1) {
+    Set-ItemProperty -Path $rdpTcpKey -Name UserAuthentication -Value 1
+}
+
+# Enable the firewall rule group for Remote Desktop - RemoteFX
+Set-NetFirewallRule -Group $remotefxRuleGroup -Enabled True
+
+# Enable the firewall rule group for Remote Desktop
+Set-NetFirewallRule -Group $remoteDesktopRuleGroup -Enabled True
+
+# Exit the PowerShell interpreter
+exit
